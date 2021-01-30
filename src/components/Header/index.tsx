67c132429fa4c05ee29e762df836d1b3684f7912
eliasallex <elliasallex@gmail.com>
2021-01-30 20:21:51 -0300

    Feat: Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,40 @@
 import React from "react";
 
-import { Container } from "./styles";
+import {
+  Container,
+  Logo,
+  Controller,
+  SearchIcon,
+  UserGroup,
+  UserIcon,
+} from "./styles";
 
 const Header: React.FC = () => {
-  return <Container>Interface reproduzida com a Rocketseat</Container>;
+  return (
+    <Container>
+      <Logo>NWoFicial</Logo>
+
+      <Controller>
+        <form action="#">
+          <input
+            required
+            type="search"
+            id="search"
+            name="search"
+            placeholder="Busque por camisetas e muito mais"
+          />
+
+          <button type="submit">
+            <SearchIcon />
+          </button>
+        </form>
+
+        <UserGroup>
+          <UserIcon />
+        </UserGroup>
+      </Controller>
+    </Container>
+  );
 };
 
 export default Header;
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { HiOutlineSearch, HiOutlineUserCircle } from "react-icons/hi";
 
 export const Container = styled.div`
   width: 100%;
@@ -8,5 +9,71 @@ export const Container = styled.div`
 
   display: flex;
   align-items: center;
+  /* justify-content: center; */
+`;
+
+export const Logo = styled.div`
+  font-weight: 600;
+  font-size: 29px;
+  max-width: 200px;
+  padding: 6px 10px;
+`;
+
+export const Controller = styled.div`
+  width: 100%;
+  margin-left: 30px;
+  max-width: 900px;
+  margin: auto;
+
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  > form {
+    display: flex;
+
+    height: 40px;
+    max-width: 500px;
+    width: 100%;
+
+    background: var(--color-white);
+    border-radius: 2px;
+    box-shadow: var(--panel-shadow);
+
+    > input {
+      height: 100%;
+      width: 100%;
+      padding: 3px 12px;
+      font-size: 16px;
+      font-weight: normal;
+    }
+
+    > button {
+      padding: 12px;
+      border-left: 2px solid rgba(0, 0, 0, 0.2);
+    }
+  }
+`;
+
+export const SearchIcon = styled(HiOutlineSearch)`
+  flex-shrink: 0;
+  height: 16px;
+  width: 16px;
+`;
+
+export const UserGroup = styled.div`
+  width: 32px;
+  height: 32px;
+
+  border-radius: 50%;
+  border: 0.5px solid rgba(0, 0, 0, 0.3);
+  display: flex;
+  align-items: center;
   justify-content: center;
+`;
+
+export const UserIcon = styled(HiOutlineUserCircle)`
+  flex-shrink: 0;
+  height: 26px;
+  width: 26px;
 `;
