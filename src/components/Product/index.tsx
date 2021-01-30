adf81f16830e45ad3e98bb70f0b9abb7dff77669
eliasallex <elliasallex@gmail.com>
2021-01-29 22:33:48 -0300

    feat: SellerInfo ini

--- src/components/Product/index.tsx
+++ src/components/Product/index.tsx
@@ -1,13 +1,101 @@
-import React from 'react';
+import React from "react";
 
-import { Container } from './styles';
+import tshirtImage from "../../assets/tshirt.png";
+import SellerInfo from "../SellerInfo";
+
+import {
+  Container,
+  Row,
+  Panel,
+  Column,
+  Gallery,
+  Section,
+  Description,
+} from "./styles";
 
 const Product: React.FC = () => {
   return (
     <Container>
-      <h1>Product</h1>
+      <Row>
+        <a href="/">Compartilhar</a>
+        <a href="/">Vender um igual</a>
+      </Row>
+
+      <Panel>
+        <Column>
+          <Gallery>
+            <img src={tshirtImage} alt="T-Shirt" />
+          </Gallery>
+
+          <Info />
+        </Column>
+
+        <Column>
+          {/* <ProductAction /> */}
+          <SellerInfo />
+
+          <WarrantySection></WarrantySection>
+          <WarrantySection></WarrantySection>
+          <WarrantySection></WarrantySection>
+        </Column>
+      </Panel>
     </Container>
   );
 };
+
+const WarrantySection = () => (
+  <Section>
+    <h4>Garantia</h4>
+
+    <div>
+      <span>
+        <p className="title">Compra garantida com o lorem Ipsum</p>
+        <p className="description">
+          Receba o produto que está espreando ou devolvemos o seu dinheiro
+        </p>
+      </span>
+
+      <span>
+        <p className="title">Garantia do vendedor</p>
+        <p className="description">Sem garantia</p>
+      </span>
+    </div>
+
+    <a href="/">Saiba mais sobre garantia</a>
+  </Section>
+);
+
+const Info = () => (
+  <Description>
+    <h2>Descrição</h2>
+
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+      commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+      cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+      est laborum.
+      <br />
+      <br />
+      Itens inclusos: <br />
+      - 1x LED <br />
+      - 1x LED <br />
+      - 1x LED <br />
+      - 1x LED <br />
+      - 1x LED <br />
+      - 1x LED <br />
+      <br />
+      <br />
+      Nibh tellus molestie nunc non blandit massa enim. Morbi tristique senectus
+      et netus et. Consectetur adipiscing elit duis tristique sollicitudin nibh.
+      At erat pellentesque adipiscing commodo elit at imperdiet dui accumsan.
+      Morbi enim nunc faucibus a pellentesque sit amet. Porttitor rhoncus dolor
+      purus non enim. Eget mauris pharetra et ultrices neque ornare aenean
+      euismod elementum. Urna neque viverra justo nec.
+    </p>
+  </Description>
+);
 
 export default Product;
---
+++ src/components/SellerInfo/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { Container } from "./styles";
+
+const SellerInfo: React.FC = () => {
+  return (
+    <Container>
+      <h2>SellerInfo</h2>
+    </Container>
+  );
+};
+
+export default SellerInfo;
