11a85bda62d05f6b9dd857bf16a05861d24d2558
eliasallex <elliasallex@gmail.com>
2021-01-29 23:13:54 -0300

    feat: ProductAction ini

--- src/components/SellerInfo/index.tsx
+++ src/components/SellerInfo/index.tsx
@@ -1,11 +1,62 @@
 import React from "react";
 
-import { Container } from "./styles";
+import {
+  Container,
+  Title,
+  LocationCard,
+  LocationIcon,
+  ReputationCard,
+  ReputationThermometer,
+  ReputationRow,
+  SupportIcon,
+  ClockIcon,
+  More,
+} from "./styles";
 
 const SellerInfo: React.FC = () => {
   return (
     <Container>
-      <h2>SellerInfo</h2>
+      <Title>Informações sobre o vendedor</Title>
+
+      <LocationCard>
+        <LocationIcon />
+
+        <div>
+          <p>Localização</p>
+          <strong>São paulo, São paulo</strong>
+        </div>
+      </LocationCard>
+
+      <ReputationCard>
+        <ReputationThermometer>
+          <li />
+          <li />
+          <li />
+          <li />
+          <li className="active" />
+        </ReputationThermometer>
+
+        <ReputationRow>
+          <div>
+            <strong>598</strong>
+            <span>vendas nos últimos 4 mêses</span>
+          </div>
+          <div>
+            <strong>
+              <SupportIcon />
+            </strong>
+            <span>presta um bom atendimento</span>
+          </div>
+          <div>
+            <strong>
+              <ClockIcon />
+            </strong>
+            <span>vendas nos últimos mêses</span>
+          </div>
+        </ReputationRow>
+      </ReputationCard>
+
+      <More href="/">Ver mais dados do vendedor</More>
     </Container>
   );
 };
